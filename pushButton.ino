47b41d2dabe3881f22c9e687a9191e4c76843f3a
mjjsnyder <mjjsnyder@gmail.com>
2014-02-03 00:15:13 -0600

    Update
    Added Power Relay functionality.
    Updated comments for case and line consistency.

--- pushButton.ino
+++ pushButton.ino
@@ -3,7 +3,6 @@
  The circuit:
  * pushbutton attached to pin 2 from +5V
  * 10K resistor attached to pin 2 from ground
- * LED attached from pin 13 to ground
  
  Created 1/18/2014 
  by Mark Snyder
@@ -13,45 +12,59 @@ Based on example code by Tom Igoe:
  */
 
 // Declaring LED and Pushbutton location constants
-const int  buttonPin = 2;    // the pin that the pushbutton is attached to
-const int ledPin = 13;       // the pin that the LED is attached to
+const int buttonPin = 2;    // Oushbutton connected to pin 2
+const int ledPin = 13;      // Utilizing onboard LED on pin 13
+const int relayPin = 4;     // Power relay connected to pin 4
 
 // Declaring counter variables
-int buttonState = 0;         // current state of the button
-int lastButtonState = 0;     // previous state of the button
-int ledState = 0;            // current state of the LED
+int buttonState = 0;         // Current state of the button
+int lastButtonState = 0;     // Previous state of the button
+int ledState = 0;            // Current state of the LED
+int relayState = 0;          // Current state of the power relay
 
 void setup() {
-  // initialize the button pin as a input:
+  // Initialize the button pin as a input:
   pinMode(buttonPin, INPUT);
-  // initialize the LED as an output:
+  // Initialize the LED and power relay as an output:
   pinMode(ledPin, OUTPUT);
+  pinMode(relayPin, OUTPUT);
+  
+  //Initializes the power relay in the 'off' position
+  //NOTE: Power relay must be pulled LOW to activate and HIGH to deactivate
+  digitalWrite(relayPin, HIGH);
 }
 
 void loop() {
-  // read the pushbutton input pin - either HIGH or LOW
+  // Read the pushbutton input pin - either HIGH or LOW
   buttonState = digitalRead(buttonPin);
 
-  // compare the buttonState to its previous state
+  // Compare the buttonState to its previous state
   if (buttonState != lastButtonState) {
-    // if the state has changed, check to see if the button went from off to on:
+    // If the state has changed, check to see if the button went from off to on:
     if (buttonState == HIGH) {
-      // if button went from off to on, flop ledState
+      // if button went from off to on, flop ledState and relayState
       ledState = !(ledState | 0);
+      relayState = !(relayState | 0);
         
-      //read ledState and turn on LED if ledState = 1. turn off if anything else 
+      //Read ledState and turn on LED if ledState = 1. Turn off if anything else 
       if (ledState == 1){
         digitalWrite(ledPin, HIGH); 
       }
       else {
           digitalWrite(ledPin, LOW);
+      }
+      
+      //Read relayState and turn on power relay if ledState = 1. Turn off if anything else
+      if (relayState == 1){
+        digitalWrite(relayPin, LOW); 
+      }
+      else {
+          digitalWrite(relayPin, HIGH);
       } 
       
-
     }
   
-  // save the current state as the last state, 
-  //for next time through the loop
+  // save the current state as the last state, for next time through the loop
   lastButtonState = buttonState;
   
   }
